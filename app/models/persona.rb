3a93b612f8d8ee41955c4bf7723b616ef74de298
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-10-13 14:58:47 -0500

    + Puedo consultar al ws por rfc

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -5,12 +5,18 @@ class Persona <  ActiveResource::Base
   self.element_name = "persona"
 
   set_primary_key "id_persona"
-  #self.format = :j
-
+  
   def self.find_by_curp(curp)
-    Persona.get(:show_by_curp, :curp => curp) if curp
+      Persona.get(:show_by_curp, :curp => curp) if curp
   end
 
+  def self.find_by_rfc(rfc)
+      Persona.get(:show_by_rfc, :rfc => rfc) if rfc 
+  end
+
+  def self.update_attributes!(attributes)
+     self.put(attributes)
+  end
 
   def nombre_completo
      "#{self.per_nombre} #{self.per_paterno} #{self.per_materno}"
