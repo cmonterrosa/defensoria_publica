42422b3e62bac2bfe94cb975eb29efa7f166112b
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-10-19 11:42:25 -0500

    + Ajuste de operaciones CRUD para audiencias

--- app/controllers/audiencias_controller.rb
+++ app/controllers/audiencias_controller.rb
@@ -2,11 +2,44 @@ class AudienciasController < ApplicationController
   before_filter :login_required
 
   def index
-    
+    @audiencias = Audiencia.find :all
   end
 
   def new_or_edit
       @audiencia = (params[:id])? Audiencia.find(params[:id]) : Audiencia.new
+  end
+
+  def save
+    @audiencia = (params[:id])? Audiencia.find(params[:id]) : Audiencia.new
+    @audiencia.update_attributes(params[:audiencia])
+    @audiencia.persona = (params[:persona] && params[:persona][:per_curp]) ? Persona.find(:first, :conditions => ["per_curp =  ?", params[:persona][:per_curp]]) : nil
+    @audiencia.persona ||= Persona.new(params[:persona])
+    if @audiencia.save && @audiencia.persona.save
+      flash[:notice] = "Audiencia registrada correctamente"
+      redirect_to :controller => "audiencias"
+    end
+  end
+
+  def show
+    @audiencia = Audiencia.find(params[:id])
+  end
+
+  def destroy
+    @audiencia = Audiencia.find(params[:id])
+    if @audiencia.destroy
+      flash[:notice] = "Registro eliminado correctamente"
+    else
+      flash[:error] = "No se pudo eliminar, verifique"
+    end
+    redirect_to :action => "index"
+  end
+
+  def get_datos_personales
+     if params[:persona_per_curp].size >= 15
+      @persona = Persona.find(:first, :conditions => ["per_curp like ?", "#{params[:persona_per_curp]}%"])
+      @persona ||= Persona.new
+     end
+     return render(:partial => 'datos_personales', :layout => false) if request.xhr?
   end
 
 
--- db/migrate/20151016161432_create_audiencias.rb
+++ db/migrate/20151016161432_create_audiencias.rb
@@ -2,14 +2,16 @@ class CreateAudiencias < ActiveRecord::Migration
   def self.up
     create_table :audiencias do |t|
       t.datetime :fechahora_solicitud, :fechahora_atencion
-      t.integer :turno, :persona_id, :defensor_id, :estado_audiencia_id
+      t.integer :turno, :defensor_id, :estado_audiencia_id
       t.string :asunto, :limit => 500
       t.string :observaciones, :limit => 300
+      t.string :procedencia, :limit => 140
+      t.string :persona_id, :limit => 36
       t.boolean :activo
       t.timestamps
     end
     add_index :audiencias, :defensor_id, :name => "audiencias_defensor"
-    add_index :audiencias, :persona_id, :name => "audiencias_defensor"
+    add_index :audiencias, :persona_id, :name => "audiencias_persona"
     add_index :audiencias, :turno, :name => "audiencias_turno"
   end
 
