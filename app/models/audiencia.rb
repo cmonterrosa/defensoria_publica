cb650abaf216d5fbd877dabd94381247d1b2a143
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-10-19 15:30:49 -0500

    + Agregue metodo que genera consecutivo de turno

--- app/models/audiencia.rb
+++ app/models/audiencia.rb
@@ -1,3 +1,16 @@
+require 'date'
+
 class Audiencia < ActiveRecord::Base
   belongs_to :persona
+
+  before_save :create_turno, :if => "self.fechahora_solicitud"
+  
+
+  def create_turno
+    maximo = Audiencia.maximum(:turno, :conditions => ["fechahora_solicitud between ? AND ?", "#{self.fechahora_solicitud.strftime('%Y-%m-%d 00:00:01')}", "#{self.fechahora_solicitud.strftime('%Y-%m-%d 23:59:59')}"])
+    STDOUT.puts "Turno maximo: #{maximo}"
+    self.turno ||= (maximo && maximo > 0)? maximo += 1 : 1
+  end
+
+  
 end
