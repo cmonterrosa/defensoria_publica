27653f10a562dfcc974e05e7ad452bc766ed81af
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-10-19 11:43:30 -0500

    + Incorpore biblioteca personalizada de javascript

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,10 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+/* Action New_or_edit, controller audiencias */
+$(function() {
+    $j('#audiencia_fechahora_solicitud').datetimepicker({
+        dayOfWeekStart : 1,
+        lang:'es'
+    });
+});
