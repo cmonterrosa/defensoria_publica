3986422402bf67537c8c68a6130113ce025401f5
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-10-20 10:44:11 -0500

    + Cambie tipo de dato a fecha

--- db/migrate/20151016161432_create_audiencias.rb
+++ db/migrate/20151016161432_create_audiencias.rb
@@ -1,7 +1,8 @@
 class CreateAudiencias < ActiveRecord::Migration
   def self.up
     create_table :audiencias do |t|
-      t.datetime :fechahora_solicitud, :fechahora_atencion
+      t.datetime :fechahora_atencion
+      t.date :fecha
       t.integer :turno, :defensor_id, :estado_audiencia_id
       t.string :asunto, :limit => 500
       t.string :observaciones, :limit => 300
@@ -13,6 +14,7 @@ class CreateAudiencias < ActiveRecord::Migration
     add_index :audiencias, :defensor_id, :name => "audiencias_defensor"
     add_index :audiencias, :persona_id, :name => "audiencias_persona"
     add_index :audiencias, :turno, :name => "audiencias_turno"
+     add_index :audiencias, :fecha, :name => "audiencias_fecha"
   end
 
   def self.down
