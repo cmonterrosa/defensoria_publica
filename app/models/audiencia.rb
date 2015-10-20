6581010b91729614c9f90bfc884c0b7a3103ac37
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-10-20 09:41:00 -0500

    + Ajuste en funcion que genera numero de turno

--- app/models/audiencia.rb
+++ app/models/audiencia.rb
@@ -9,7 +9,7 @@ class Audiencia < ActiveRecord::Base
   def create_turno
     maximo = Audiencia.maximum(:turno, :conditions => ["fechahora_solicitud between ? AND ?", "#{self.fechahora_solicitud.strftime('%Y-%m-%d 00:00:01')}", "#{self.fechahora_solicitud.strftime('%Y-%m-%d 23:59:59')}"])
     STDOUT.puts "Turno maximo: #{maximo}"
-    self.turno ||= (maximo && maximo > 0)? maximo += 1 : 1
+    self.turno ||= (maximo && maximo > 0)? maximo + 1 : 1
   end
 
   
