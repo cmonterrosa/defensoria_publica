eac9616f9f837a9be56359d61bf516058de87033
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-10-26 11:19:09 -0600

    + Apunto directamente a tabla de personas

--- app/models/persona.rb
+++ app/models/persona.rb
@@ -1,7 +1,14 @@
+##################################
+# Modelo que conecta directamente a tabla de persona
+#
+##################################
+
 class Persona < ActiveRecord::Base
   include UUIDHelper
-  #set_table_name "pr_persona"
+  establish_connection "persona"
+  set_table_name "pr_persona"
   set_primary_key "id_persona"
+  
   
   def nombre_completo
      "#{self.per_nombre} #{self.per_paterno} #{self.per_materno}"
