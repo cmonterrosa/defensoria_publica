dc8166af46787826aa44acc3d1e2157d4f2368b0
Gerardo Aguilar <eriolsama@gmail.com>
2015-11-09 14:09:21 -0600

    agregue el migrate de marginacion

---
+++ db/migrate/20151026213646_create_marginacions.rb
@@ -0,0 +1,18 @@
+class CreateMarginacions < ActiveRecord::Migration
+  def self.up
+    create_table :marginacions do |t|
+    	t.column :descripcion, :string, :limit=>40 
+    end
+
+    Marginacion.create(:descripcion=>"MUY BAJA") unless Marginacion.find_by_descripcion("MUY BAJA")
+    Marginacion.create(:descripcion=>"BAJA") unless Marginacion.find_by_descripcion("BAJA")
+    Marginacion.create(:descripcion=>"MEDIA") unless Marginacion.find_by_descripcion("MEDIA")
+    Marginacion.create(:descripcion=>"ALTA") unless Marginacion.find_by_descripcion("ALTA")
+    Marginacion.create(:descripcion=>"MUY ALTA") unless Marginacion.find_by_descripcion("MUY ALTA")
+
+  end
+
+  def self.down
+    drop_table :marginacions
+  end
+end
