dc58703a8b2f4b5955818490f53dbe26e54d95a9
Carlos Monterrosa <cmonterrosa@gmail.com>
2015-12-15 13:53:56 -0600

    + Incluimos librerias en environment

--- config/environment.rb
+++ config/environment.rb
@@ -38,4 +38,6 @@ Rails::Initializer.run do |config|
   # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
+  config.middleware.use "PDFKit::Middleware", :print_media_type => true
+  config.gem 'will_paginate', :version => '~> 2.3.16'
 end
