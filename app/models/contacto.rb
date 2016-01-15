b70a2f59c66cabcfae79fab90e024c3e68c3d694
Gerardo Aguilar <eriolsama@gmail.com>
2016-01-15 15:35:10 -0600

    modificacion en las vistas de participantes

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -3,10 +3,23 @@ class Contacto < ActiveRecord::Base
   set_table_name "pr_contacto"
   #belongs_to :persona, class_name => "Persona", :foreign_key => "id_persona"
   belongs_to :persona
+  belongs_to :tipo_contacto
   set_primary_key "id_contacto"
+  
 
 	# def persona
 	#  return Persona.find(self.fk_persona)
 	# end
+	def correo
+		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 2"])
+	end
+
+	def telefono_casa
+		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 4"])
+	end
+
+	def celular
+		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 1"])
+	end
 
 end
--- app/models/tipo_contacto.rb
+++ app/models/tipo_contacto.rb
@@ -1,4 +1,7 @@
 class TipoContacto < ActiveRecord::Base
   establish_connection "persona"
   set_table_name "pr_tipo_contacto"
+
+  has_many :contactos
+  has_many "contactos", :foreign_key =>"fk_tipo"
 end
