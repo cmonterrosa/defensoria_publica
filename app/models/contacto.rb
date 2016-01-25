344707ba9077bce252e0df60bfb3a6f6c955c56a
Carlos Monterrosa <cmonterrosa@gmail.com>
2016-01-25 15:57:07 -0600

    + Agregue modelo que guarda tipos de contacto vinculados a personas

--- app/models/contacto.rb
+++ app/models/contacto.rb
@@ -2,24 +2,43 @@ class Contacto < ActiveRecord::Base
   establish_connection "persona"
   set_table_name "pr_contacto"
   #belongs_to :persona, class_name => "Persona", :foreign_key => "id_persona"
-  belongs_to :persona
+  belongs_to :persona, :foreign_key => "fk_persona"
   belongs_to :tipo_contacto
   set_primary_key "id_contacto"
-  
+
+  named_scope :telefono_celular, :conditions => {:fk_tipo => 1}
+  named_scope :correo_electronico, :conditions => {:fk_tipo => 2}
+  named_scope :direccion, :conditions => {:fk_tipo => 3}
+  named_scope :telefono_casa, :conditions => {:fk_tipo => 2}
+
+  before_create :time_stamping, :make_active
+
+
+  def make_active
+    self.con_activo_reg ||= 1
+  end
+
+  def time_stamping
+    self.con_modi ||= Time.now
+  end
+
+  def descripcion
+    self.con_parametro
+  end
 
 	# def persona
 	#  return Persona.find(self.fk_persona)
 	# end
-	def correo
-		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 2"])
-	end
-
-	def telefono_casa
-		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 4"])
-	end
-
-	def celular
-		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 1"])
-	end
+#	def correo
+#		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 2"])
+#	end
+#
+#	def telefono_casa
+#		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 4"])
+#	end
+#
+#	def celular
+#		find(:first, :select => "con_parametro", :conditions => ["fk_tipo = 1"])
+#	end
 
 end
--- app/models/tipo_contacto.rb
+++ app/models/tipo_contacto.rb
@@ -1,7 +1,7 @@
 class TipoContacto < ActiveRecord::Base
   establish_connection "persona"
-  set_table_name "pr_tipo_contacto"
-
-  has_many :contactos
+  set_table_name "cat_tipo_contacto"
+  set_primary_key "id_tipo_contacto"
+  #has_many :contactos
   has_many "contactos", :foreign_key =>"fk_tipo"
 end
