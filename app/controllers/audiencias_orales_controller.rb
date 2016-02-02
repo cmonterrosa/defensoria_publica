2567de4a4a08e9a9a6edc6f869a6576fe85d8bcc
Carlos Monterrosa <cmonterrosa@gmail.com>
2016-02-02 11:58:27 -0600

    + Ajustes en controlador y vista de vista de audiencias orales

--- app/controllers/audiencias_orales_controller.rb
+++ app/controllers/audiencias_orales_controller.rb
@@ -1,7 +1,7 @@
-######################################
-# Controlador que administra las audiencias inicial, intermedia y
-# de juicio oral
-######################################
+###########################################
+#  Controlador que administra las acciones básicas de
+#  las audiencias inicial, intermedia y   de juicio oral
+#############################################
 
 class AudienciasOralesController < ApplicationController
   require_role :defensor, :for_all_except => :show
@@ -22,6 +22,10 @@ class AudienciasOralesController < ApplicationController
       @tipos_audiencias = TipoAudiencia.all
       @organos = Organo.all
       @jueces = Juez.find(:all, :conditions => ["activo = ?", true])
+      if current_user.has_role?(:defensor)
+       @defensores = Defensor.find(:all, :conditions => ["persona_id = ?", current_user.persona.id]) if current_user.persona
+      end
+      @defensores ||= Defensor.find(:all, :conditions => ["activo = ?", true])
       @audiencia.fecha ||= Time.now.strftime("%Y/%m/%d")
       @tramite = @audiencia.tramite if @audiencia
       @tramite ||= Tramite.find(params[:t]) if params[:t]
@@ -57,6 +61,42 @@ class AudienciasOralesController < ApplicationController
       @audiencia = AudienciaOral.find(params[:id])
       @tramite = @audiencia.tramite if @audiencia
       @tramite ||= Tramite.find(params[:t]) if params[:t]
-  end
+    end
+
+    ########################################
+    # Acciones que se llaman desde la vista de calendario
+    #
+    ########################################
+
+    def show_window
+      @audiencia = AudienciaOral.find(params[:id])
+      @tramite = @audiencia.tramite if @audiencia
+      @tramite ||= Tramite.find(params[:t]) if params[:t]
+      render :partial => "show", :layout => "only_javascript"
+    end
+
+    def cancel_window
+      @audiencia = AudienciaOral.find(params[:id])
+      @tramite = @audiencia.tramite if @audiencia
+      @tramite ||= Tramite.find(params[:t]) if params[:t]
+      if @audiencia.cancelar(current_user)
+        flash[:notice] = "Audiencia cancelada correctamante"
+      else
+        flash[:error] = "No se pudo cancelar, verifique"
+      end
+      render :partial => "show", :layout => "only_javascript"
+    end
+
+     def reactivar_window
+      @audiencia = AudienciaOral.find(params[:id])
+      @tramite = @audiencia.tramite if @audiencia
+      @tramite ||= Tramite.find(params[:t]) if params[:t]
+      if @audiencia.reactivar
+        flash[:notice] = "Audiencia reactivada correctamante"
+      else
+        flash[:error] = "No se pudo reactivar, verifique"
+      end
+      render :partial => "show", :layout => "only_javascript"
+    end
 
 end
