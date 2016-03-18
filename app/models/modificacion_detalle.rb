98bbb76f4d8a491b0e9aa1719b869ec38c762ea8
Carlos Monterrosa <cmonterrosa@gmail.com>
2016-03-18 14:01:49 -0600

    + Ajuste de condiciones de detalle de journal

--- app/models/modificacion_detalle.rb
+++ app/models/modificacion_detalle.rb
@@ -1,6 +1,17 @@
 class ModificacionDetalle < ActiveRecord::Base
   belongs_to :modificacion
+  before_save :check_id
+  validates_uniqueness_of :id
+  validates_presence_of :modificacion_id, :if => "self.id"
 
+  def check_id
+    unless self.id
+        maximo=  self.class.maximum(:id)
+        id = 1 unless maximo
+        id ||= maximo.to_i + 1
+        self.id = id
+     end
+  end
   
  def descripcion_campo(campo)
     (self.class.find_by_sql("SELECT a.COLUMN_COMMENT AS comment
@@ -8,8 +19,4 @@ class ModificacionDetalle < ActiveRecord::Base
     WHERE a.TABLE_NAME = '#{self.class.table_name}' AND
    a.COLUMN_NAME='#{campo}';").first.comment) if campo
  end
-
- 
-
-
 end
