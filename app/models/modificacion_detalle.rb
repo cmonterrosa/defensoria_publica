35319771f0abd05ec1e315e95499cdd04213bfce
Carlos Monterrosa <cmonterrosa@gmail.com>
2016-04-06 10:43:14 -0500

    + Ajuste de condiciones

--- app/models/modificacion_detalle.rb
+++ app/models/modificacion_detalle.rb
@@ -26,10 +26,10 @@ class ModificacionDetalle < ActiveRecord::Base
  end
 
  def valor_anterior
-  if self.campo =~ /\w+id$/
+  if self.campo =~ /\w+id$/ && self.old_value
      row = eval("#{self.campo.gsub("_id", "").camelize}.find(#{self.old_value})")
-     va = (row && row.persona) ? row.persona.nombre_completo : nil
-     va ||= (row && row.descripcion) ? row.descripcion : self.old_value
+     va =  (row && row.respond_to?(:descripcion)) ? row.descripcion : nil
+     va ||= (row && row.respond_to?(:persona_id)) ? row.persona.nombre_completo : self.old_value
      return va
   else
       return self.old_value
@@ -37,10 +37,10 @@ class ModificacionDetalle < ActiveRecord::Base
  end
 
  def valor_nuevo
-   if self.campo =~ /\w+id$/
+   if self.campo =~ /\w+id$/ && self.value
      row = eval("#{self.campo.gsub("_id", "").camelize}.find(#{self.value})")
-     vn = (row && row.persona) ? row.persona.nombre_completo : nil
-     vn ||= (row && row.descripcion) ? row.descripcion : self.value
+     vn = (row && row.respond_to?(:descripcion)) ? row.descripcion : nil
+     vn ||= (row && row.respond_to?(:persona_id)) ? row.persona.nombre_completo : self.value
      return vn
   else
       return self.value
