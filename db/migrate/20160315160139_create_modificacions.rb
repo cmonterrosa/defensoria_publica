faf15612ee1c7d61521d1b4659101789ca95bcc9
Carlos Monterrosa <cmonterrosa@gmail.com>
2016-04-06 09:45:43 -0500

    + Nueva columna

--- db/migrate/20160315160139_create_modificacions.rb
+++ db/migrate/20160315160139_create_modificacions.rb
@@ -4,9 +4,11 @@ class CreateModificacions < ActiveRecord::Migration
       t.column :id_objeto, :integer
       t.column :clase, :string, :limit => 30
       t.column :user_id, :integer
+      t.column :is_created, :boolean
       t.timestamps
     end
     add_index :modificacions, [:user_id], :name => "modificacions_user"
+    add_index :modificacions, [:id_objeto, :clase, :is_created], :name => "modificacions_primer_registro"
   end
 
   def self.down
