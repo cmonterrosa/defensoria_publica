e0de6da38cc1bfa493be370dfe0c5957165806f5
Carlos Monterrosa <cmonterrosa@gmail.com>
2016-09-01 11:53:05 -0500

    + Ajustes en controlador principal

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,48 +5,64 @@ class HomeController < ApplicationController
     redirect_to :action => "dispatch"
   end
 
+  # Método que se encarga de enlazar usuario con el menu que le corresponde de acuerdo al role
     def dispatch
       @usuario=current_user
       if @usuario.has_role?(:admin)
           return render(:partial => 'admin', :layout => "content")
+      elsif @usuario.has_role?(:directivo)
+          return render(:partial => 'directivo', :layout => "content")
       elsif @usuario.has_role?(:jefedefensor)
           return render(:partial => 'jefedefensor', :layout => "content")
-      elsif @usuario.has_role?(:defensor)
-          return render(:partial => 'defensor', :layout => "content")
+      elsif @usuario.has_role?(:defensorpenal)
+          return render(:partial => 'defensorpenal', :layout => "content")
       elsif @usuario.has_role?(:defensorapoyo)
+          return render(:partial => 'defensor', :layout => "content")
+      elsif @usuario.has_role?(:defensor)
           return render(:partial => 'defensor', :layout => "content")
       elsif @usuario.has_role?(:solicitante)
           return render(:partial => 'solicitante', :layout => "content")
+      elsif @usuario.has_role?(:recepcion)
+          return render(:partial => 'recepcion', :layout => "content")
       else
           return render(:partial => 'default', :layout => "content")
       end
     end
 
+    # Devuelve los datos de configuracion de la cuenta actual
     def myaccount
       @user = current_user
     end
 
-    def get_datos_personales
+
+    def get_datos_personales_minimos
+        get_datos_personales('minimals')
+    end
+
+    # Obtiene los datos personales, o crea un registro nuevo de persona
+    def get_datos_personales(tipo=nil)
       @persona ||= Persona.new
       #@contacto ||= Contacto.new
+      @tipo = tipo
+      partial_persona = (@tipo)? 'datos_personales_minimos' : 'datos_personales'
       if params[:persona_per_curp] && params[:persona_per_curp].size >= 6
         if @persona = Persona.find(:first, :conditions => ["per_curp like ?", "#{params[:persona_per_curp]}%"])
           @contacto = Contacto.find(:first, :conditions => ["fk_persona = ?", @persona.id]) if @persona
-          return render(:partial => 'datos_personales', :layout => false) if request.xhr?
+          return render(:partial => partial_persona, :layout => false) if request.xhr?
         else
           if @personas = Persona.search(params[:persona_per_curp])
             if @personas.empty?
-              return render(:partial => 'datos_personales', :layout => false) if request.xhr?
+              return render(:partial => partial_persona, :layout => false) if request.xhr?
             else
               return render(:partial => 'seleccion_persona', :layout => false) if request.xhr?
             end
             else
-              return render(:partial => 'datos_personales', :layout => false) if request.xhr?
+              return render(:partial => partial_persona, :layout => false) if request.xhr?
             end
         end
         else
             @persona = Persona.find(params[:persona_id]) if (params[:persona_id] && params[:persona_id].size == 36)
-            return render(:partial => 'datos_personales', :layout => false) if request.xhr?
+            return render(:partial => partial_persona, :layout => false) if request.xhr?
         end
       end
 
