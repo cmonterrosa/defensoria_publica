1c101349054c3a9c04525981e62ae8e1f56c087c
Carlos Monterrosa <cmonterrosa@gmail.com>
2017-01-05 11:42:30 -0600

    + Tipo de juicios

--- db/migrate/20151026165045_create_tipo_juicios.rb
+++ db/migrate/20151026165045_create_tipo_juicios.rb
@@ -2,7 +2,8 @@ class CreateTipoJuicios < ActiveRecord::Migration
   def self.up
     create_table :tipo_juicios do |t|
       t.column :materia_id, :integer
-      t.column :descripcion, :string
+      t.column :clave, :string, :limit => 6
+      t.column :descripcion, :string, :limit => 80
       end
   end
 
